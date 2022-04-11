84b164f0bdfe4a5b52983f8134c86c95a35d4b83
Aleksandr Mikhailov <alex01236@gmail.com>
2022-04-11 21:07:57 +0300

    Deleted quiz 4

--- quiz-4/questions/2.js
+++
@@ -1,9 +0,0 @@
-/**
- * Function to get array of all active users (not suspended users)
- * - should return a promise
- * @param {*} data - see shape in ../../data.example.json
- * @returns Array of users
- */
-const getActiveUsers = data => new Promise(res => { res(data.filter(v => !v.isSuspended)); });
-
-module.exports = getActiveUsers;
